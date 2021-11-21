5b0d7a2a75f486fa2dfb8e3876b328bc96e882a1
Zahar25 <zpyshny@gmail.com>
2021-11-21 14:12:59 +0200

    Create homework2.py

---
+++ homework2.py
@@ -0,0 +1,3 @@
+last_number = str(input("Enter number: "))
+list_in_dict = [(10, 11, 13), (12, 14, 16), (18, 20, 25, 30), (31, 33), (100, )]
+print (random_number[:-1] + (last_number,) for random_number in list_in_dict)
